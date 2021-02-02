a528c5d5e40126e8d855bf1ad0516d42fd98232b
pratik982505 <gharatp%5@gmail.com>
2021-02-02 17:38:54 +0530

    Refactor:Used function to calculate wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,9 @@
 console.log("Welcome to Employee Wage Computation Program");
     const FULL_DAY_WAGE = 20;
     const FULL_DAY_WORKING_HOURS = 8;
-    const PART_TIME_WORKING_HOURS = 4;
-    
-    //function attendence() {
+    const PART_TIME_WORKING_HOURS = 4;  
+
+    function attendence() {
         let employee_Attendence = Math.floor(Math.random() * 3);
     
         switch (employee_Attendence) {
@@ -16,4 +16,7 @@ console.log("Welcome to Employee Wage Computation Program");
             case 2:
                 console.log("Employee is Absent \t Daily Wage = 0");
                 break;
-        }
+       }
+    }
+
+attendence();
