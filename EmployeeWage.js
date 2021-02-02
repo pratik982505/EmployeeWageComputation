15acb24cd504695e1f5be5a294429bfaef7d73a0
pratik982505 <gharatp%5@gmail.com>
2021-02-02 17:59:19 +0530

    Added: Condition to Calculate Employee Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,8 +2,12 @@ console.log("Welcome to Employee Wage Computation Program");
     const FULL_DAY_WAGE = 20;
     const FULL_DAY_WORKING_HOURS = 8;
     const PART_TIME_WORKING_HOURS = 4;  
+    const MAX_WORKING_DYAS =20;
+    let totalWorkingDays =0;
+
 
     function attendence() {
+        for (totalWorkingDays = 1; totalWorkingDays <= MAX_WORKING_DYAS;  totalWorkingDays++) {
         let employee_Attendence = Math.floor(Math.random() * 3);
     
         switch (employee_Attendence) {
@@ -16,7 +20,8 @@ console.log("Welcome to Employee Wage Computation Program");
             case 2:
                 console.log("Employee is Absent \t Daily Wage = 0");
                 break;
+           }
        }
     }
-
 attendence();
+console.log("Total Working Days = " + totalWorkingDays );
