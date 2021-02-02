8232ffd874d6be4a146f408eac27e1a96b79a67a
pratik982505 <gharatp%5@gmail.com>
2021-02-02 17:07:12 +0530

    Added:Wage Calculation Per Day

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,19 @@
 console.log("Welcome to Employee Wage Computation Program");
-
-let employee_Attendence = Math.floor(Math.random() * 2);
-if (employee_Attendence === 1){
-    console.log("Employee is Present");
-    }
-    else {
-        console.log("Employee is Absent");
-    }
+    const FULL_DAY_WAGE = 20;
+    const FULL_DAY_WORKING_HOURS = 8;
+    const PART_TIME_WORKING_HOURS = 4;
+    
+    //function attendence() {
+        let employee_Attendence = Math.floor(Math.random() * 3);
+    
+        switch (employee_Attendence) {
+            case 0:
+                console.log("Employee is Present \t Daily Wage = " + FULL_DAY_WAGE * FULL_DAY_WORKING_HOURS);
+                break;
+            case 1:
+                console.log("Employee is Present Part Time \t Daily Wage = " + FULL_DAY_WAGE * PART_TIME_WORKING_HOURS);
+                break;
+            case 2:
+                console.log("Employee is Absent \t Daily Wage = 0");
+                break;
+        }
