d4a7b975a7fe52974a6cf45b62ece3cf90738edd
pratik982505 <gharatp%5@gmail.com>
2021-02-02 18:23:31 +0530

    Added:Code for wage calculation till cindition

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,20 +2,24 @@ console.log("Welcome to Employee Wage Computation Program");
     const FULL_DAY_WAGE = 20;
     const FULL_DAY_WORKING_HOURS = 8;
     const PART_TIME_WORKING_HOURS = 4;  
+    const  MAX_WORKING_HOURS = 100;
     const MAX_WORKING_DYAS =20;
     let totalWorkingDays =0;
+    let totalWorkingHours =0;
 
 
     function attendence() {
-        for (totalWorkingDays = 1; totalWorkingDays <= MAX_WORKING_DYAS;  totalWorkingDays++) {
+        for (totalWorkingDays = 1; totalWorkingDays <= MAX_WORKING_DYAS && totalWorkingHours <= MAX_WORKING_HOURS ;  totalWorkingDays++) {
         let employee_Attendence = Math.floor(Math.random() * 3);
     
         switch (employee_Attendence) {
             case 0:
                 console.log("Employee is Present \t Daily Wage = " + FULL_DAY_WAGE * FULL_DAY_WORKING_HOURS);
+                totalWorkingHours += PART_TIME_WORKING_HOURS;
                 break;
             case 1:
                 console.log("Employee is Present Part Time \t Daily Wage = " + FULL_DAY_WAGE * PART_TIME_WORKING_HOURS);
+                totalWorkingHours += FULL_DAY_WORKING_HOURS;
                 break;
             case 2:
                 console.log("Employee is Absent \t Daily Wage = 0");
@@ -25,3 +29,4 @@ console.log("Welcome to Employee Wage Computation Program");
     }
 attendence();
 console.log("Total Working Days = " + totalWorkingDays );
+console.log("Total Working Hours = " +  totalWorkingHours);
